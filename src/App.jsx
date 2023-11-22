d7f9900719f025e4bd2a26a12428ce9ec3e0f5d1
Bogdan <brasskazov@gmail.com>
2023-11-22 18:19:13 +0300

    feat: profile

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import "./stylesheets/App.scss";
 
 import RootLayout from "./layouts/RootLayout.jsx";
 import CoursesLayout from "./layouts/CoursesLayout.jsx";
+import ProfileLayout from "./layouts/CoursesLayout.jsx";
+
 import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
 import About from "./pages/About.jsx";
 import Courses from "./pages/Courses.jsx";
-import Login from "./pages/Login.jsx";
 import Course from "./pages/Course.jsx";
+import Profile from "./pages/Profile.jsx";
 
 import { Route, Routes } from "react-router-dom";
 function App() {
@@ -17,12 +20,18 @@ function App() {
             <Routes>
                 <Route path="/" element={<RootLayout />}>
                     <Route index element={<Home />} />
+                    <Route path="login" element={<Login />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="profile" element={<ProfileLayout />}>
+                        <Route index element={<Profile />} />
+                        {/* <Route path="edit" element={<EditProfile />} /> */}
+                        {/* <Route path=":userId" element={<Doer />} /> */}
+                    </Route>
+
                     <Route path="courses" element={<CoursesLayout />}>
                         <Route index element={<Courses />} />
                         <Route path=":courseId" element={<Course />} />
                     </Route>
-                    <Route path="about" element={<About />} />
-                    <Route path="login" element={<Login />} />
                 </Route>
             </Routes>
         </BrowserRouter>
